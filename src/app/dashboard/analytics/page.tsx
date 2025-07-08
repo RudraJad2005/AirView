a5227fe0302c54beffada4e67abdcbca48a96d81
Rudra Jadhav <rudrajad2005@gmail.com>
2025-07-08 11:33:09 +0000

    can you remove the unused files in the project folder.

--- src/app/dashboard/analytics/page.tsx
+++
@@ -1,8 +0,0 @@
-import { AnalyticsClient } from '@/components/dashboard/analytics-client';
-import { getLocationsData } from '@/lib/data';
-
-export default function AnalyticsPage() {
-  const locations = getLocationsData();
-  const states = [...new Set(locations.map(l => l.state))];
-  return <AnalyticsClient states={states} />;
-}
